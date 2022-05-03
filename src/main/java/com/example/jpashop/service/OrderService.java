cad62283917c773e101ae0f684fb5e499ca9a431
elzlelzl <62757384+elzlelzl@users.noreply.github.com>
2022-05-04 00:13:42 +0900

    Update OrderService.java

--- src/main/java/com/example/jpashop/service/OrderService.java
+++ src/main/java/com/example/jpashop/service/OrderService.java
@@ -47,7 +47,7 @@ public class OrderService {
     }
     /** 주문 검색 */
     public List<Order> findOrders(OrderSearch orderSearch) {
-        return orderRepository.findAll(orderSearch);
+        return orderRepository.findAllByCriteria(orderSearch);
     }
 
 }
